98258e87b6aa21959f95fb6cde5a95263bf5d18e
Erica Schimmel <ericaschimmel01@gmail.com>
2020-04-28 19:18:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 let wakeDog = function(dogName, dogBreed) {
   console.log(`Wake ${dogName} the ${dogBreed}`);
   return `Wake ${dogName} the ${dogBreed}`;
-}
+};
 
-function leashDog(dogName, dogBreed) {
+let leashDog = function(dogName, dogBreed) {
   console.log(`Leash ${dogName} the ${dogBreed}`);
   return `Leash ${dogName} the ${dogBreed}`;
-}
+};
 
-function walkToPark(dogName, dogBreed) {
+let walkToPark = function(dogName, dogBreed) {
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
   return `Throw the frisbee for ${dogName} the ${dogBreed}`;
 }
 
-function throwFrisbee(dogName, dogBreed) {
+let throwFrisbee = function(dogName, dogBreed) {
   console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
   return `Throw the frisbee for ${dogName} the ${dogBreed}`;
-}
+};
 
-function walkHome(dogName, dogBreed) {
+let walkHome = function walkHome(dogName, dogBreed) {
   console.log(`Walk home with ${dogName} the ${dogBreed}`);
   return `Walk home with ${dogName} the ${dogBreed}`;
 }
