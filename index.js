270c049fbb673ea509ac61abedde36d08cbf7b9a
Erica Schimmel <ericaschimmel01@gmail.com>
2020-04-28 19:03:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 function wakeDog(dogName, dogBreed) {
   console.log(`Wake #{dogName} the #{dogBreed}`);
 }
+
+function leashDog(dogName, dogBreed) {
+  console.log(`Wake #{dogName} the #{dogBreed}`);
+}
+
+function walkToPark() {
+  
+}
+
+function throwFrisbee() {
+  
+}
+
+function walkHome() {
+  
+}
+
+function unleashDog(dogName, dogBreed) {
+  console.log(`Wake #{dogName} the #{dogBreed}`);
+}
