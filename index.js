7b9f6a331940ed355a698bee68b21315d41bbbde
Erica Schimmel <ericaschimmel01@gmail.com>
2020-04-28 19:20:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let leashDog = function(dogName, dogBreed) {
 
 let walkToPark = function(dogName, dogBreed) {
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
-  return `Throw the frisbee for ${dogName} the ${dogBreed}`;
+  return`Walk to the park with ${dogName} the ${dogBreed}`;
 };
 
 let throwFrisbee = function(dogName, dogBreed) {
